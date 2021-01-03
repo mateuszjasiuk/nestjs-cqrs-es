6e0c963dfe14248f85cf70ba176522181ef8de2f
Mateusz Jasiuk <mateusz.jasiuk@instapage.com>
2021-01-03 17:21:26 +0100

    feat: step1 - then to async await

--- src/block/infrastructure/block-created.handler.ts
+++ src/block/infrastructure/block-created.handler.ts
@@ -7,27 +7,24 @@ import { BlockCreatedEvent } from '../domain';
 export class BlockCreatedHandler implements IEventHandler<BlockCreatedEvent> {
   constructor(@Inject('EventStore') private eventStore: HTTPClient) {}
 
-  handle(event: BlockCreatedEvent) {
+  async handle(event: BlockCreatedEvent) {
     const payload = event.getPayload();
 
-    this.eventStore
-      .writeEvents(`block-${payload.id}`, [
-        {
-          eventId: event.getId(),
-          eventType: 'BlockCreated',
-          data: {
-            metadata: { version: event.getVersion() },
-            ...payload,
-          },
+    await this.eventStore.writeEvents(`block-${payload.id}`, [
+      {
+        eventId: event.getId(),
+        eventType: 'BlockCreated',
+        data: {
+          metadata: { version: event.getVersion() },
+          ...payload,
         },
-      ])
-      .then(() =>
-        console.log(
-          `Event with id: ${event.getId()} has been written to stream: block-${
-            payload.id
-          }`,
-        ),
-      )
-      .catch((e) => console.error(e));
+      },
+    ]);
+
+    console.log(
+      `Event with id: ${event.getId()} has been written to stream: block-${
+        payload.id
+      }`,
+    );
   }
 }
